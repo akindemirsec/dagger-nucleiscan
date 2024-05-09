b112654d1544ecdc08636043f6811966235188ec
Akın Demir <128565483+akindemirsec@users.noreply.github.com>
2024-05-09 14:29:13 +0300

    dagger-nuclei scan

---
+++ main.go
@@ -0,0 +1,36 @@
+// A generated module for Nucleidagger functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+type Nucleidagger struct{}
+
+func (d *Nucleidagger) RunNucleiScan(ctx context.Context, targets []string) (string, error) {
+	cmd := dag.Container().
+		From("projectdiscovery/nuclei").
+		WithExec([]string{"-silent", "-t", strings.Join(targets, ",")})
+
+	output, err := cmd.Stdout(ctx)
+	if err != nil {
+		return "", fmt.Errorf("error running Nuclei scan: %v", err)
+	}
+
+	return output, nil
+}
